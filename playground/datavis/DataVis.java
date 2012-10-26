9df4d2d7b74be6981179a04481c1ef3a0b2f20f7
Stephane Beniak <stephane.beniak@gmail.com>
2012-10-26 00:36:23 -0400

    Scrolly-gradienty plot now working. Apologies for probably ruining build.xml, but I suspect no one is using it for now.

--- playground/datavis/DataVis.java
+++ playground/datavis/DataVis.java
@@ -1,4 +1,3 @@
-import panoia.*;
 import java.io.*;
 import java.util.*;
 import processing.core.*;
@@ -9,21 +8,27 @@ public class DataVis extends PApplet {
 	PFont font;
 	
 	ArrayList<CarAccident> carAccidents;
+	
+	ScrollingPlot temperaturePlot;
 
 	public void setup() {
-		size(displayWidth, displayHeight);
+		size(1024, 768);
 		background(0);
-
-		smooth();
 		
 		font = createFont("Arial", 16, true);
+		
+		temperaturePlot = new ScrollingPlot(this, new PVector(100, 100), -100, 100, color(0, 0, 255), color(255, 0, 0));
 		
 		carAccidents = CarAccident.ParseCsv(this);
 	}
 
 	public void draw() {
+		
 		background(0);
-
+	
+		temperaturePlot.update();
+		temperaturePlot.draw();
+		
 		/*
 		CarAccident test = carAccidents.get(1);
 		
@@ -35,7 +40,7 @@ public class DataVis extends PApplet {
 		*/
 		
 		for (CarAccident accident : carAccidents) {
-			accident.draw(45.5, -73.6, 1600, 900, 180, 90, 0);
+			accident.draw(45.5, -73.6, 1024, 768, 180, 90, 0);
 		}
 	}
 
