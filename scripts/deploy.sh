33bd3ba2792d467843e8e93153866e6e512491f8
Severin Smith <info@smallbutdigital.com>
2013-02-26 12:38:39 -0500

    Updated deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,6 @@ cd ../prototype/server/
 go build -o rter main.go
 mv ./rter /usr/local/share/rter/
 cp -r templates/ /usr/local/share/rter/
+cp -r resources/ /usr/local/share/rter/
 chgrp -R rter /usr/local/share/rter/
 sudo /etc/init.d/rter.sh start
