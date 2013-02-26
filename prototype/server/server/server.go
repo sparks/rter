bec840b9547dda551c5a8a97db1f352a29a5c4c6
Severin Smith <info@smallbutdigital.com>
2013-02-26 12:35:47 -0500

    Moved resources out of templates

--- prototype/server/server/server.go
+++ prototype/server/server/server.go
@@ -10,7 +10,7 @@ var rterDir = os.Getenv("RTER_DIR")
 
 var ImagePath = filepath.Join(rterDir, "images")
 var TemplatePath = filepath.Join(rterDir, "templates")
-var ResourcePath = filepath.Join(rterDir, "templates", "resources")
+var ResourcePath = filepath.Join(rterDir, "resources")
 
 var phoneIDValidator = regexp.MustCompile("^[a-zA-Z0-9_]+$")
 
