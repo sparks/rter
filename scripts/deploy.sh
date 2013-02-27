2bed8e752f15b2c4adde89d5fbdf709b8873d709
Severin Smith <info@smallbutdigital.com>
2013-02-26 23:42:43 -0500

    More tweaks to deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,8 @@ cd ../prototype/server/
 go build -o rter main.go
 mv ./rter /usr/local/share/rter/
 cp -r templates/ /usr/local/share/rter/
+cp -r images/ /usr/local/share/rter/
 cp -r resources/ /usr/local/share/rter/
 chgrp -R rter /usr/local/share/rter/
+chmod g+rw -R  /usr/local/share/rter/
 sudo /etc/init.d/rter.sh start
