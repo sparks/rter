1bcc7660a380aeaa38b46b663ac6fb493b7c3ae0
Severin Smith <info@smallbutdigital.com>
2013-03-15 10:54:13 -0400

    Omit empty fields in JSON encoding

--- prototype/server/src/rter/data/taxonomy.go
+++ prototype/server/src/rter/data/taxonomy.go
@@ -5,17 +5,17 @@ import (
 )
 
 type Term struct {
-	Term string
+	Term string `json:",omitempty"`
 
 	Automated bool
 	AuthorID  int64
 
-	UpdateTime time.Time
+	UpdateTime time.Time `json:",omitempty"`
 }
 
 type TermRanking struct {
-	Term    string
-	Ranking string
+	Term    string `json:",omitempty"`
+	Ranking string `json:",omitempty"`
 
-	UpdateTime time.Time
+	UpdateTime time.Time `json:",omitempty"`
 }
