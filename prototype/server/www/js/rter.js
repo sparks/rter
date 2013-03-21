ae42c04196606a696212e3132522e638e9b4f5ff
Severin Smith <info@smallbutdigital.com>
2013-03-21 02:19:44 -0400

    Make tag-cloud a directive of taxonomy

--- prototype/server/www/js/rter.js
+++ prototype/server/www/js/rter.js
@@ -2,21 +2,7 @@ angular.module('rter', ['ui.bootstrap', 'items', 'termview', 'alerts', 'taxonomy
 
 .controller('TabsCtrl', function($scope, Taxonomy, TermViewRemote) {
 	$scope.termViews = TermViewRemote.termViews;
-
 	TermViewRemote.addTermView({Term: ""});
-	$scope.terms = Taxonomy.query(function() {
-		$scope.countMax = 0;
-
-		angular.forEach($scope.terms, function(val) {
-			if($scope.countMax < val.Count) $scope.countMax = val.Count;
-		});
-	});
-
-	$scope.addTermView = TermViewRemote.addTermView;
-
-	$scope.termFontSize = function(term) {
-		return term.Count/$scope.countMax*30;
-	};
 })
 
 .directive('eatClick', function() {
--- prototype/server/www/js/taxonomy/taxonomy.js
+++ prototype/server/www/js/taxonomy/taxonomy.js
@@ -1,4 +1,4 @@
-angular.module('taxonomy', ['ngResource'])
+angular.module('taxonomy', ['ngResource', 'termview'])
 
 .factory('Taxonomy', function ($resource) {
 	var Taxonomy = $resource(
@@ -10,4 +10,33 @@ angular.module('taxonomy', ['ngResource'])
 	);
 
 	return Taxonomy;
+})
+
+
+.controller('TagCloudCtrl', function($scope, TermViewRemote, Taxonomy) {
+	$scope.terms = Taxonomy.query(function() {
+		$scope.countMax = 0;
+
+		angular.forEach($scope.terms, function(val) {
+			if($scope.countMax < val.Count) $scope.countMax = val.Count;
+		});
+	});
+
+	$scope.addTermView = TermViewRemote.addTermView;
+
+	$scope.termFontSize = function(term) {
+		return term.Count/$scope.countMax*30;
+	};
+})
+
+.directive('tagCloud', function(ItemCache) {
+	return {
+		restrict: 'E',
+		scope: {},
+		templateUrl: '/template/taxonomy/tag-cloud.html',
+		controller: 'TagCloudCtrl',
+		link: function(scope, element, attrs) {
+
+		}
+	};
 });
