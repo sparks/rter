4b173b30958d79f67616d497f996fe89bff0229f
Severin Smith <info@smallbutdigital.com>
2013-03-30 21:50:41 -0400

    Fixing deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 sudo /etc/init.d/rter.sh stop
 cd ../prototype/server/
-go build -o rter main.go
-mv ./rter /usr/local/share/rter/
+go install rter
+cp ./bin/rter /usr/local/share/rter/
 cp -r www/ /usr/local/share/rter/
+cp -r uploads/ /usr/local/share/rter/
 chgrp -R rter /usr/local/share/rter/
 chmod g+rw -R  /usr/local/share/rter/
 sudo /etc/init.d/rter.sh start
