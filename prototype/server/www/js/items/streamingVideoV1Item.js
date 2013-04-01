de69f4b7b74763c88290e8047f022761a89e99f6
Severin Smith <info@smallbutdigital.com>
2013-04-01 17:11:01 -0400

    Added an ng-poster directive for video, using ng-src in video aswell
    Avoiding 404s when we try and loading uninterpolated URIs

--- prototype/server/www/js/items/streamingVideoV1Item.js
+++ prototype/server/www/js/items/streamingVideoV1Item.js
@@ -38,4 +38,18 @@ angular.module('streamingVideoV1Item', [
 
 		}
 	};
+})
+
+.directive('ngPoster', function() {
+	return {
+		priority: 99, // it needs to run after the attributes are interpolated
+		link: function(scope, element, attr) {
+			attr.$observe('ngPoster', function(value) {
+				if (!value)
+					return;
+
+				attr.$set('poster', value);
+			});
+		}
+	};
 });
