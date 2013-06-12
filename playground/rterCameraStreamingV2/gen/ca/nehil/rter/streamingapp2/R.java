a1e4c1d84e10d960819b13a0b15db97bf4549b06
nehiljain <jain.nehil@gmail.com>
2013-06-12 12:22:12 -0400

    nothing major

--- playground/rterCameraStreamingV2/gen/ca/nehil/rter/streamingapp2/R.java
+++ playground/rterCameraStreamingV2/gen/ca/nehil/rter/streamingapp2/R.java
@@ -28,20 +28,25 @@ public final class R {
         public static final int semi_black=0x7f020001;
     }
     public static final class id {
-        public static final int action_forgot_password=0x7f090008;
+        public static final int action_forgot_password=0x7f09000b;
         public static final int login=0x7f090006;
         public static final int login_form=0x7f090003;
         public static final int login_status=0x7f090001;
         public static final int login_status_message=0x7f090002;
         public static final int password=0x7f090005;
+        public static final int record_layout=0x7f090008;
+        public static final int recorder_control=0x7f09000a;
         public static final int sign_in_button=0x7f090007;
         public static final int surface_camera=0x7f090000;
+        public static final int textView1=0x7f090009;
         public static final int username=0x7f090004;
     }
     public static final class layout {
-        public static final int activity_first=0x7f030000;
-        public static final int activity_login=0x7f030001;
-        public static final int activity_token=0x7f030002;
+        public static final int acitivity_streaming=0x7f030000;
+        public static final int activity_first=0x7f030001;
+        public static final int activity_login=0x7f030002;
+        public static final int activity_token=0x7f030003;
+        public static final int javacvtext_main=0x7f030004;
     }
     public static final class menu {
         public static final int login=0x7f080000;
